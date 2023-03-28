a15117c4899bd3c7b5849251c61062310f99b686
johnathan <johnathanboyd5@gmail.com>
2023-03-28 00:05:20 -0500

    added router

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from 'router';
 
 const app = express();
 const dotenv = require('dotenv')
@@ -30,3 +31,5 @@ const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PAS
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());

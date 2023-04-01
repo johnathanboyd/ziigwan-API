4b55df10b90375567e391fa3c99ad42784a2654f
johnathan <johnathanboyd5@gmail.com>
2023-04-01 00:14:22 -0500

    added update

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteUserById, getUsers} from '../db/users';
+import {deleteUserById, getUserById, getUsers} from '../db/users';
 
 export const getAllUsers = async (req:express.Request, res: express.Response) => {
   try {
@@ -11,6 +11,25 @@ export const getAllUsers = async (req:express.Request, res: express.Response) =>
     return res.sendStatus(400);
   }
 };
+export const updateUser =async (req:express.Request, res: express.Response) => {
+  try {
+    const { id } = req.params;
+    const { username} = req.body;
+    if(!username){
+      return res.sendStatus(400)
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.sendStatus(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
 
 export const deleteUser = async (req: express.Request, res: express.Response) => {
   try {
